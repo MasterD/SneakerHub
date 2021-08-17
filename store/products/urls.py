24fdffc2099aaec8406288961d7823c30ed05925
Tiaan <tiaan@assemble.co.za>
2021-08-17 10:32:53 +0200

    add cartapp

--- store/products/urls.py
+++ store/products/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 from . import views
-from .views import CreateProduct, ProductDetail, ProductListView, UpdateProductView
+from .views import CreateProduct, ProductDetail, ProductListView, UpdateProductView, DeleteProductView
 
 urlpatterns = [
     path('product/', ProductListView.as_view(), name='view_products'),
     path('product/new/', CreateProduct.as_view(), name='product-create'),
     path('product/<int:pk>/', ProductDetail.as_view(), name='product-detail'),
     path('product/<int:pk>/update/', UpdateProductView.as_view(), name='product-update'),
+    path('product/<int:pk>/delete/', DeleteProductView.as_view(), name='product-delete'),
 ]
